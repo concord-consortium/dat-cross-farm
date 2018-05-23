bc5e4ab786e3abe37140ada3f09af1c39117830a
Kirk Swenson <kswenson@concord.org>
2018-05-23 15:13:12 -0700

    Bold results in end-of-season dialog

--- src/components/end-season-dialog.tsx
+++ src/components/end-season-dialog.tsx
@@ -25,8 +25,16 @@ export default class EndSeasonDialog extends React.Component<IProps, IState> {
           { yearStats } = this.props,
           { initial, final } = { ...yearStats },
           { simulationYear, countCorn: initialCorn, countEggs: initialEggs } = { ...initial },
-          finalCorn = final && final.countCorn || 0,
-          finalEggs = final && final.countEggs || 0,
+          finalCorn = final ? final.countCorn : null,
+          finalEggs = final ? final.countEggs : null,
+          cornPlanted = <span className='dialog-text-bold'>{initialCorn || 0}</span>,
+          cornHarvested = <span className='dialog-text-bold'>{finalCorn || 0}</span>,
+          cornYield = initialCorn
+                        ? `${Math.round(100 * (finalCorn || 0) / initialCorn)}%`
+                        : null,
+          cornYieldSpan = <span className='dialog-text-bold'>{cornYield || 0}</span>,
+          initialEggsSpan = <span className='dialog-text-bold'>{initialEggs || 0}</span>,
+          finalEggsSpan = <span className='dialog-text-bold'>{finalEggs || 0}</span>,
           invasionMsg = !initialEggs && finalEggs
                           ? <div>Oh no! There's been a rootworm invasion! Some of the corn has been eaten and some rootworm eggs have been laid in the soil.</div>
                           : null;
@@ -34,8 +42,8 @@ export default class EndSeasonDialog extends React.Component<IProps, IState> {
       <div className={'dialog-container' + displayClass} onClick={this.toggleVisibility}>
         <div className="dialog-text">
           <div style={{ fontWeight: 'bold' }}>Year {simulationYear + 1} Results</div>
-          <div>Corn planted: {initialCorn || 0}, harvested: {finalCorn}</div>
-          <div>Rootworm eggs initial: {initialEggs || 0}, final: {finalEggs}</div>
+          <div>Corn planted: {cornPlanted}, harvested: {cornHarvested}, yield: {cornYieldSpan}</div>
+          <div>Rootworm eggs initial: {initialEggsSpan}, final: {finalEggsSpan}</div>
           {invasionMsg}
           <div>How much corn should Jonah plant for next year to maximize his corn crop?</div>
         </div>
