5f8df3f714fd73bcec2f0163aced342fe4b83178
Kirk Swenson <kswenson@concord.org>
2018-05-25 14:20:52 -0700

    Eliminate unused global from setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,19 +1,16 @@
 // Load our script into JSDom
 // cf. https://stackoverflow.com/a/19491110
-const script =  document.createElement("script");
+const script = document.createElement("script");
 script.type = "text/javascript";
 import fs = require("fs");
 const requireJSFile = fs.readFileSync("./test/vendor-test.js","utf8");
 script.innerHTML = requireJSFile;
-const head= window.document.getElementsByTagName('head')[0];
+const head = window.document.getElementsByTagName('head')[0];
 head.appendChild(script);
 
 // Allow us to add properties to 'global'
 // cf. https://stackoverflow.com/a/42304473
 const globalAny:any = global;
-
-// Mock gImages array
-globalAny.gImages = {};
 
 // Load jquery separately since it takes ~30 sec for JSDom to load it as part of the script
 import $ = require('jquery');
